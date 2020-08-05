b79895be4112e2af6b6e5d714cdafc0199da1a67
ErickCReis <erickcorreareis@gmail.com>
2020-08-05 14:41:53 -0300

    Dia 03 - Finalizando front end e integrando com back end.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,33 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../service/api';
+
 import './styles.css'
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+  subject: string;
+  cost: number;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+  
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars1.githubusercontent.com/u/30873873?s=460&u=a8b45ac2c517dbfff21fbd98926780bf1e7f423c&v=4" alt="Erick Reis"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Erick Reis</strong>
-        <span>Matemática</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
-    <p>
-      Descrição
-      <br/><br/>
-      Detalhes da descrição
-    </p>
+
+    <p>{teacher.bio}</p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 50,00</strong>
+        <strong>R${teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a 
+        target="_blanck"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}
+      >
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   )
